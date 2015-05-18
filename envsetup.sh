b150d9a1e393fd2c3b47782c2174fae589bc4023
Michael J. Cusack <WhoNeedszZz@gmail.com>
2015-05-18 05:36:45 -0500

    Revert: prebuilt chromium:make sure to call the function after setting the environment variable

--- envsetup.sh
+++ envsetup.sh
@@ -545,13 +545,6 @@ function lunch()
 
     set_stuff_for_environment
 
-    if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
-        chromium_prebuilt
-    else
-        # Unset flag in case user opts out later on
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=""
-    fi
-
     printconfig
 }
 
